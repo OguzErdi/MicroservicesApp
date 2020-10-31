eac1f5cae5cc9720755d85df4cbb46643f8559ef
OguzErdi <erdi.erkmen@gmail.com>
2020-10-31 15:38:14 +0300

    Create Context for Redis and set ConntectionMultiplaxer to connect Redis

---
+++ src/Basket/Basket.API/Data/BasketContext.cs
@@ -0,0 +1,24 @@
+﻿using Basket.API.Data.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Data
+{
+    public class BasketContext : IBasketContext
+    {
+        //to craete connection with redis db
+        private readonly ConnectionMultiplexer _redisConnection;
+
+        //get ConnectionMultiplexer via Dependency Injection
+        public BasketContext(ConnectionMultiplexer redisConnection)
+        {
+            _redisConnection = redisConnection;
+            Redis = _redisConnection.GetDatabase();
+        }
+
+        public IDatabase Redis { get; }
+    }
+}
--- src/Basket/Basket.API/Startup.cs
+++ src/Basket/Basket.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace Basket.API
 {
@@ -24,6 +25,13 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //create ConnectionMultiplexer object given parameters
+            services.AddSingleton<ConnectionMultiplexer>(sp =>
+            {
+                var configuraitonParameter = ConfigurationOptions.Parse(Configuration.GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuraitonParameter);
+            });
+            
             services.AddControllers();
         }
 
