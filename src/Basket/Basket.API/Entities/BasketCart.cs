4ab8a3d0f888181a8c7303b0e5b8fbd4189afbb4
OguzErdi <erdi.erkmen@gmail.com>
2020-10-31 11:58:00 +0300

    entities created

---
+++ src/Basket/Basket.API/Entities/BasketCart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    //name it BasketCart because may have confilict with Basket Enum Name 
+    public class BasketCart
+    {
+        public string UserName { get; set; }
+        public List<BasketCartItem> Items { get; set; } = new List<BasketCartItem>();
+
+        public BasketCart()
+        {
+        }
+
+        public BasketCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+
+                Items.ForEach(x => totalPrice += x.Price * x.Quantitiy);
+
+                return totalPrice;
+            }
+        }
+
+    }
+}
---
+++ src/Basket/Basket.API/Entities/BasketCartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.API.Entities
+{
+    public class BasketCartItem
+    {
+        public int Quantitiy { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        //denormalized table, for higher perfromance
+        public string ProductName { get; set; }
+
+    }
+}
