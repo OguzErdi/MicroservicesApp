6ec48efb0fee29255ee7a7bef3625b77c7ae3c65
OguzErdi <erdi.erkmen@gmail.com>
2020-10-31 17:37:06 +0300

    remove typo

--- src/Basket/Basket.API/Repositories/BasketRepository.cs
+++ src/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -3,7 +3,6 @@ using Basket.API.Entities;
 using Basket.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using Newtonsoft.Json.;
 using System;
 using System.Collections.Generic;
 using System.Linq;
