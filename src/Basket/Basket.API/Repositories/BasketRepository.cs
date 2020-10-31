26d94378d35265c5155a0f0a5f7d7a3a6343c280
OguzErdi <erdi.erkmen@gmail.com>
2020-10-31 16:49:53 +0300

    create repositroy

---
+++ src/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,54 @@
+﻿using Basket.API.Data.Interfaces;
+using Basket.API.Entities;
+using Basket.API.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IBasketContext _context;
+
+        public BasketRepository(IBasketContext context)
+        {
+            _context = context;
+        }
+
+        //to perform lazy loading use Task
+        public async Task<BasketCart> GetBasket(string userName)
+        {
+            var basket = await _context.Redis.StringGetAsync(userName);
+
+            if (basket.IsNullOrEmpty)
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<BasketCart>(basket);
+        }
+
+        public async Task<BasketCart> UpdateBasket(BasketCart basketCart)
+        {
+            string jsonBasketCart = JsonConvert.SerializeObject(basketCart);
+            var isUpdated = await _context.Redis.StringSetAsync(basketCart.UserName, jsonBasketCart);
+
+            if (!isUpdated)
+            {
+                return null;
+            }
+
+            return await GetBasket(basketCart.UserName);
+        }
+
+        public async Task<bool> DeleteBasket(string userName)
+        {
+            return await _context.Redis.KeyDeleteAsync(userName);
+        }
+    }
+}
---
+++ src/Basket/Basket.API/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,24 @@
+﻿using Basket.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<BasketCart> GetBasket(string userName);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="basketCart">
+        /// a key-value pair which is key should be userName and value should be whole basket object as json object
+        /// </param>
+        /// <returns>
+        /// the updated basketCart value
+        /// </returns>
+        Task<BasketCart> UpdateBasket(BasketCart basketCart);
+        Task<bool> DeleteBasket(string userName);
+    }
+}
